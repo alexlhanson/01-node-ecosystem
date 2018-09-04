8733a2c5dfa22db2884ca3f362d1a915842ab4c5
alexlhanson <alexlhanson@gmail.com>
2018-09-04 16:43:13 -0700

    Add tests for non string params

---
+++ test/greet.test.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const greet = require('../lib/greet.js');
+
+test('This should return a message Hello World', () => {
+  let actual = greet.hello('World'),
+    expectedValue = 'Hello World';
+  expect(actual).toBe(expectedValue);
+})
+
+test('This should return null if wrong parameter type is passed in', () =>{
+  expect(greet.hello(1)).toBeNull();
+  expect(greet.hello(true)).toBeNull();
+  expect(greet.hello()).toBeNull();
+  expect(greet.hello([])).toBeNull();
+  expect(greet.hello({})).toBeNull();
+})
+
