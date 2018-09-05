a0d593fcdd879870dc3d6dc9fd019d0b5e7766f3
alexlhanson <alexlhanson@gmail.com>
2018-09-04 21:23:25 -0700

    Add tests and function for arithmetic

--- lab-alex/test/arithmetic.test.js
+++ lab-alex/test/arithmetic.test.js
@@ -2,9 +2,21 @@
 
 const arithmetic = require('../lib/arithmetic.js');
 
+// test('should return addition and subtraction of two numbers', () => {
+//   let actual = arithmetic.add(1, 2),
+//     expectedValue = 3;
+//   expect(actual).toBe(expectedValue);
+
+//   actual = arithmetic.sub(1, 2); 
+//   expectedValue = 1;
+//   expect(actual).toBe(expectedValue);
+
+// })
+
 test('should return null if parameters are not a number', () => {
   expect(arithmetic.add(NaN, 4)).toBeNull();
   expect(arithmetic.add(4, NaN)).toBeNull();
   expect(arithmetic.sub(NaN, 4)).toBeNull();
   expect(arithmetic.sub(4, NaN)).toBeNull();
 })
+
