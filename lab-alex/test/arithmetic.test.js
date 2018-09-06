e5289e1e6a441492cb0e4a9a3363db3003d21140
alexlhanson <alexlhanson@gmail.com>
2018-09-06 08:30:21 -0700

    Refactor tests

--- lab-alex/test/arithmetic.test.js
+++ lab-alex/test/arithmetic.test.js
@@ -2,21 +2,25 @@
 
 const arithmetic = require('../lib/arithmetic.js');
 
-test('should return addition and subtraction of two numbers', () => {
+test('should return addition of two numbers', () => {
   let actual = arithmetic.add(1, 2),
     expectedValue = 3;
   expect(actual).toBe(expectedValue);
-
-  actual = arithmetic.sub(1, 2); 
-  expectedValue = 1;
-  expect(actual).toBe(expectedValue);
-
 })
 
-test('should return null if parameters are not a number', () => {
+
+test('should return subtraction of two numbers', () => {
+  let actual = arithmetic.sub(1, 2), 
+    expectedValue = 1;
+  expect(actual).toBe(expectedValue);
+})
+
+test('add should return null if parameters are not a number', () => {
   expect(arithmetic.add(NaN, 4)).toBeNull();
   expect(arithmetic.add(4, NaN)).toBeNull();
+})
+
+test('sub should return null if parameters are not a number', () => {
   expect(arithmetic.sub(NaN, 4)).toBeNull();
   expect(arithmetic.sub(4, NaN)).toBeNull();
 })
-
